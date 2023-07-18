4fe9c430157d9bc5ecd135eaf3f5653941ad3799
Oksanarym40 <135235310+Oksanarym40@users.noreply.github.com>
2023-07-18 17:03:22 -0400

    Update Main.java
    Fixed all the requested changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,29 +4,42 @@ import java.util.Map;
 
 public class Main {
 
-        public static void main(String[]args){
-            String[]rows={"мама","тато","їж","їжак","желе"};
-            Map<Character,Integer>uniqueLetters=findUniqueLetters(rows);
-            System.out.println("Unique letters:"+uniqueLetters.keySet());
-        }
+        public class Main {
 
-        public static Map<Character,Integer>findUniqueLetters(String[]rows){
-            Map<Character,Integer>letterCounts=new HashMap<>();
+    public static void main(String[] args) {
+        String[] rows = {"мама", "тато", "їж", "їжак", "желе"};
+        List<Set<Character>> uniqueLettersList = findUniqueLetters(rows);
 
-            for(String row:rows){
-                char[]characters=row.toCharArray();
-                for(char c:characters){
-                    letterCounts.put(c,letterCounts.getOrDefault(c,0)+1);
-                }
-            }
-
-            Map<Character,Integer>uniqueLetters=new HashMap<>();
-            for(char c:letterCounts.keySet()){
-                if(letterCounts.get(c)==2){
-                    uniqueLetters.put(c,2);
-                }
-            }
-
-            return uniqueLetters;
+       
+        for (int i = 0; i < rows.length; i++) {
+            System.out.println("Unique letters " + (i + 1) + ": " + uniqueLettersList.get(i));
         }
     }
+
+    public static List<Set<Character>> findUniqueLetters(String[] rows) {
+        List<Set<Character>> uniqueLettersList = new ArrayList<>();
+
+        for (String row : rows) {
+            Map<Character, Integer> letterCounts = new HashMap<>();
+            char[] characters = row.toCharArray();
+            for (char c : characters) {
+                if (Character.isLetter(c)) {
+                    c = Character.toLowerCase(c);
+                    letterCounts.put(c, letterCounts.getOrDefault(c, 0) + 1);
+                }
+            }
+
+            Set<Character> uniqueLetters = new HashSet<>();
+            for (char c : letterCounts.keySet()) {
+                int occurrences = letterCounts.get(c);
+                if (occurrences % 2 == 0) {
+                    uniqueLetters.add(c);
+                }
+            }
+
+            uniqueLettersList.add(uniqueLetters);
+        }
+
+        return uniqueLettersList;
+    }
+}
