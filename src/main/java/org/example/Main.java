d390073da1bdab041d43f902ce8469d9104bd82e
Oksanarym40 <135235310+Oksanarym40@users.noreply.github.com>
2023-07-19 11:23:19 -0400

    Update Main.java
    Updated for latest requested changes.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,10 @@ import java.util.Map;
 
 public class Main {
 
-        public class Main {
-
     public static void main(String[] args) {
         String[] rows = {"мама", "тато", "їж", "їжак", "желе"};
         List<Set<Character>> uniqueLettersList = findUniqueLetters(rows);
 
-       
         for (int i = 0; i < rows.length; i++) {
             System.out.println("Unique letters " + (i + 1) + ": " + uniqueLettersList.get(i));
         }
@@ -18,27 +15,33 @@ public class Main {
 
     public static List<Set<Character>> findUniqueLetters(String[] rows) {
         List<Set<Character>> uniqueLettersList = new ArrayList<>();
+        Set<Character> uniqueCharactersForFirstTwoWords = new HashSet<>();
 
         for (String row : rows) {
             Map<Character, Integer> letterCounts = new HashMap<>();
-            char[] characters = row.toCharArray();
-            for (char c : characters) {
+            for (char c : row.toCharArray()) {
                 if (Character.isLetter(c)) {
                     c = Character.toLowerCase(c);
                     letterCounts.put(c, letterCounts.getOrDefault(c, 0) + 1);
                 }
             }
 
+            boolean allEvenOccurrences = letterCounts.values().stream().allMatch(count -> count % 2 == 0);
+
             Set<Character> uniqueLetters = new HashSet<>();
-            for (char c : letterCounts.keySet()) {
-                int occurrences = letterCounts.get(c);
-                if (occurrences % 2 == 0) {
-                    uniqueLetters.add(c);
-                }
+            if (allEvenOccurrences) {
+                uniqueLetters.addAll(letterCounts.keySet());
+            }
+
+            if (uniqueLettersList.size() < 2 && allEvenOccurrences) {
+                uniqueCharactersForFirstTwoWords.addAll(uniqueLetters);
             }
 
             uniqueLettersList.add(uniqueLetters);
         }
+
+        uniqueLettersList.set(0, uniqueCharactersForFirstTwoWords);
+        uniqueLettersList.set(1, uniqueCharactersForFirstTwoWords);
 
         return uniqueLettersList;
     }
